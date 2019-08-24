fdf8e8daf3d05faaab88c272e9a433f7d0bf2b82
Juan Rodriguez <jbcrodriguezsud@gmail.com>
2019-08-24 12:57:09 -0300

    task 2 finish

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -2,22 +2,23 @@
 var lista = document.getElementById("lista");
 
 //variables globales
-
 window.addEventListener('load', function () {
 
     console.log("hola termino de cargar");
-    $.ajax({
+
+    /*$.ajax({
         url: '/api/games',
         success: function(games) {
            // console.log(games);
+           console.log("se logroooo");
         },
         error: function() {
             console.log("Error, No se ha podido obtener");
         }
     })
-        .done(function( games ) {
-            console.log(games);
-        });
+    .done(function( games ) {
+        console.log(games);
+     });*/
 
     $.get("/api/games")
         .done(function (games) {
@@ -30,19 +31,17 @@ window.addEventListener('load', function () {
         .always(function() {
             console.log( "finished" );
         });
-
-
 });
 
 function cargarLista(obj){
-    var ele = "<li>";
-    for (const e in obj) {
-        ele+=obj[e];
-        ele+="</li>"
-
-        console.log(obj[e]);
-        lista.append(ele);
-    }
+    var htmlList = "";
+    obj.forEach(e => {
+              htmlList +='<li>';
+              htmlList +=new Date(e.creationDate).toLocaleString();
+              htmlList += ' ' + e.gamePlayers.map(function(p) { return p.player.email}).join(',');
+              htmlList +='</li>';
+        });
+    lista.innerHTML = htmlList;
 }
 
 
