cbc3b4e0795f517e0706a894da6ece52e2581267
Janneth <emi.perez997@gmail.com>
2019-10-03 21:27:09 -0300

    5 1 6

--- src/main/java/com/codeoftheweb/Salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/Salvo/SalvoController.java
@@ -1,11 +1,12 @@
 package com.codeoftheweb.Salvo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -19,6 +20,9 @@ public class SalvoController {
     private GamePlayerRepository gamePlayerRepository;
     @Autowired
     private PlayerRepository playerRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @RequestMapping("/games")
     public Map<String, Object> getGames(Authentication authentication) {
         Map<String, Object> dto = new LinkedHashMap<String, Object>();
@@ -50,4 +54,16 @@ public class SalvoController {
     }
 
 
+    @RequestMapping (path = "/players", method = RequestMethod.POST)
+    public ResponseEntity<Object> register(
+            @RequestParam String username, @RequestParam String password){
+        if (username.isEmpty()||password.isEmpty()) {
+            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        }
+        if (playerRepository.findByUserName(username)!= null) {
+            return new ResponseEntity<>("Name alredy in use",HttpStatus.FORBIDDEN);
+        }
+        playerRepository.save(new Player(username, passwordEncoder.encode(password)));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
